3f1793f4d471ff25fae750b29a21779890b94e0c
Dewey <hammersdrichard@gmail.com>
2020-12-22 09:37:26 -0800

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
-# Your code goes here!
+require 'pry'
+
+
+
+class Anagram
+    attr_accessor :word, :match
+
+    def initialize(word)
+       @word = word
+    end
+
+    def match(match)
+        @match = []
+        match.map do |match_word|
+            if match_word.split("").sort == @word.split("").sort
+                @match << match_word
+            end
+        end
+        @match
+    end
+end
+
+diaper = Anagram.new('diaper')
+diaper.match(%w(hello world zombies pants dipper))
+
